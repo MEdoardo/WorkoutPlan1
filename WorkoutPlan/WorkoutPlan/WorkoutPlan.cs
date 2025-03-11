bf55ec0f43901157b25ec435e4d2a3b90b1fed32
MEdoardo <85733020+MEdoardo@users.noreply.github.com>
2025-03-11 19:32:00 +0200

    Add files via upload

---
+++ WorkoutPlan/WorkoutPlan/WorkoutPlan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutPlan
+{
+    public class WorkoutPlan
+    {
+        public string PlanName { get; set; }
+        public string Description { get; set; }
+        public List<Exercise> Exercises { get; set; }
+
+        public WorkoutPlan(string planName, string description)
+        {
+            PlanName = planName;
+            Description = description;
+            Exercises = new List<Exercise>();
+        }
+
+        public void AddExercise(Exercise exercise)
+        {
+            Exercises.Add(exercise);
+        }
+
+        public void ShowPlan()
+        {
+            Console.WriteLine($"Workout Plan: {PlanName}\nDescription: {Description}\nExercises:");
+            foreach (var exercise in Exercises)
+            {
+                Console.WriteLine(" - " + exercise);
+            }
+        }
+    }
+}
