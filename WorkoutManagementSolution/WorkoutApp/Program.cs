7aa01b8cbd34cc7e7727ba28eafaec0742576e0f
MEdoardo <85733020+MEdoardo@users.noreply.github.com>
2025-03-11 20:03:57 +0200

    Add files via upload

---
+++ WorkoutManagementSolution/WorkoutApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WorkoutLibrary;
+using ExerciseLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkoutPlan plan = new WorkoutPlan("Full Body Workout", "A simple full-body routine.");
+            plan.AddExercise(new Exercise("Push-ups", 15, 3));
+            plan.AddExercise(new Exercise("Squats", 20, 3));
+            plan.AddExercise(new Exercise("Plank", 60, 3)); // 60 seconds
+
+            plan.ShowPlan();
+
+            Console.ReadLine();
+        }
+    }
+}
