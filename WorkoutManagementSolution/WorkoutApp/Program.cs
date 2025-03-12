5a5809d28cb00fb58caad7eecaea82e1ca080ae2
MEdoardo <85733020+MEdoardo@users.noreply.github.com>
2025-03-12 13:27:47 +0200

    Update Program.cs

--- WorkoutManagementSolution/WorkoutApp/Program.cs
+++ WorkoutManagementSolution/WorkoutApp/Program.cs
@@ -15,7 +15,7 @@ namespace WorkoutApp
             WorkoutPlan plan = new WorkoutPlan("Full Body Workout", "A simple full-body routine.");
             plan.AddExercise(new Exercise("Push-ups", 15, 3));
             plan.AddExercise(new Exercise("Squats", 20, 3));
-            plan.AddExercise(new Exercise("Plank", 60, 3)); // 60 seconds
+            plan.AddExercise(new Exercise("Plank", 60, 3)); 
 
             plan.ShowPlan();
 
